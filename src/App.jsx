fb678e42e96bd594e5c547dc64edad0fbe28b319
Jake Tripp <jake@jake.local>
2019-01-20 15:50:23 -0600

    Prepare for GH pages. Make app always current (use current year).

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            deadline: 'December 25, 2018',
+            deadline: `December 25, ${new Date().getFullYear()}`,
             newDeadline: ''
         };
     }
